c22bb05257692cdfc021fce29be9871e991f9eb8
hiikayu <ehhkayud@gmail.com>
2026-04-27 20:06:02 +0800

    prototype

--- taiga/sketch.js
+++ taiga/sketch.js
@@ -2,6 +2,9 @@
 // works everytime ?
 let scene = 0
 let scroll = 0;
+let scroll2 = 0
+let scroll3 = 0
+let scroll4 = 0
 let startstarter = false
 let paper = []
 let paperAnimation = 0
@@ -43,21 +46,29 @@ let doggie
 //fifth scene
 // bear encounter animation - clickinb
 
+let bear = []
+let bearAnimation = 0
 
 // sixth scene 
 // dog ran away omgg / scrolling
+let bg2 = [];
 
 //seventh scene
 // waiting, maybe forest animation - clicking
-
+let scene7
 // eight scene
 // dog came back with policeman - polaroid scrolling !!!!????
+let bg3 = []
 
 // ninth scene
 // her small hands found policeman - animation clicking
 
-// tenth scene
-// yupiee aftermath - scrolling !
+let grass = []
+let grassAnimation = 0
+
+
+// last pic of karina in the 11th scene
+let karinapic
 
 
 function preload() {
@@ -85,17 +96,33 @@ function preload() {
   for (let i = 1; i <= 3; i++) {
     berries.push(loadImage("images/03/" + i + ".png"));
   }
+  for (let i = 1; i <= 4; i++) {
+    bg2.push(loadImage("images/06/" + i + ".png"));
+  }
+  for (let i = 1; i <= 9; i++) {
+    bear.push(loadImage("images/05/" + i + ".png"));
+  }
+  for (let i = 1; i <= 4; i++) {
+    bg3.push(loadImage("images/08/" + i + ".png"));
+  }
+  for (let i = 1; i <= 7; i++) {
+    grass.push(loadImage("images/09/" + i + ".png"));
+  }
 
   // add loops later after finishing drawings !
 
   doggie = loadImage("images/04/1.png")
 
+  //beomg = loadImage("images/beom.jfif")
+  scene7 = loadImage("images/07/1.png")
+  karinapic = loadImage("images/10/1.png")
   backtrack = loadSound("sound/trees.mp3");
   flip = loadSound("sound/page-flip.mp3");
 }
 
 function setup() {
-  createCanvas(1280, 959);
+  let canvas = createCanvas(1280, 959);
+  canvas.parent("p5-canvas-container");
   frameRate(8)
   backtrack.loop();
   backtrack.setVolume(0.3);
@@ -107,6 +134,8 @@ function draw() {
   } else {
     drawtaiga()
   }
+
+  console.log(scroll)
 }
 
 function drawtaiga() {
@@ -119,16 +148,20 @@ function drawtaiga() {
 
   } else {
 
-    if (scene == 0 || scene == 4) {
-      scene = floor(scroll / (3 * width))
-    }
 
-    let sceneScroll = scroll % (4 * width)
+    let sceneScroll = scroll % (3 * width)
+    console.log(sceneScroll)
+
 
     if (scene == 0) {
+      // scene = floor(scroll / (3 * width))
       for (let i = 0; i < bg.length; i++) {
         image(bg[i], i * width - sceneScroll, 0);
         image(karina[karinaRunningFrame], 0, 0);
+
+        if (scroll >= (bg.length - 1) * width) {
+          scene = 1;
+        }
       }
     } else if (scene == 1) {
 
@@ -152,24 +185,78 @@ function drawtaiga() {
       image(doggie, 0, 0)
 
     } else if (scene == 4) {
-      //bear
+      //bear animation 23.04 today
+      image(bear[bearAnimation], 0, 0)
+      bearAnimation = (bearAnimation + 1) % bear.length
 
     } else if (scene == 5) {
 
+      let scene2Scroll = scroll2 % (3 * width)
+
+      // scene = floor (scroll2 / (3 * width)) the problem is here. i left the click for that
+      //if (scroll2 > 4000) {
+      //scene = 6
+      //}
+      //scene = floor(scroll2 / (3 * width))
+
+
+      for (let i = 0; i < bg2.length; i++) {
+        image(bg2[i], i * width - scene2Scroll, 0);
+      }
+
+      if (scroll2 >= (bg2.length - 1) * width) {
+        scene = 6;
+      }
+
+    } else if (scene == 6) {
+      image(scene7, 0, 0)
+
+    } else if (scene == 7) {
+
+      let scene3Scroll = scroll3 % (4 * width)
+
+      // scene = floor (scroll2 / (3 * width)) the problem is here. i left the click for that
+
+      for (let i = 0; i < bg3.length; i++) {
+        image(bg3[i], i * width - scene3Scroll, 0);
+      }
+
+      if (scroll3 >= (bg3.length - 1) * width) {
+        scene = 8;
+      }
     }
 
+
+    else if (scene == 8) {
+      image(grass[grassAnimation], 0, 0)
+      grassAnimation = (grassAnimation + 1) % grass.length
+
+    }
+
+    else if (scene == 9) {
+      // ending naimation.
+      image(karinapic, 0, 0)
+    }
+
+    push()
+    tint(200, 100)
+    image(paper[paperAnimation], 0, 0)
+    paperAnimation = (paperAnimation + 1) % paper.length
+    pop()
+
   }
-
-  push()
-  tint(200, 100)
-  image(paper[paperAnimation], 0, 0)
-  paperAnimation = (paperAnimation + 1) % paper.length
-  pop()
-
 }
 
 function mouseWheel() {
   scroll += 20;
+
+  if (scene == 5) {
+    scroll2 += 20
+  } else if (scene == 7) {
+    scroll3 += 20
+  } else if (scene == 9) {
+    scroll4 += 20
+  }
 
   karinaRunningFrame++;
   if (karinaRunningFrame >= karina.length) {
@@ -183,11 +270,13 @@ function drawMenu() {
 }
 
 function mousePressed() {
+
   startstarter = true
 
   if (scene == 1) {
     scene = 2;
     flip.play();
+
   } else if (scene == 2) {
     scene = 3;
     flip.play();
@@ -195,9 +284,16 @@ function mousePressed() {
   } else if (scene == 3) {
     scene = 4;
     flip.play();
-
   } else if (scene == 4) {
-    flip.play
-    scene = 5
+    scene = 5;
+    flip.play();
+  }
+  else if (scene == 6) {
+    scene = 7
+    flip.play();
+
+  } else if (scene == 8) {
+    scene = 9
+    flip.play();
   }
 }
