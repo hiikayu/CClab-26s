de527585199f587d78706a84dbaffff238b16509
hiikayu <ehhkayud@gmail.com>
2026-05-04 16:54:29 +0800

    scene 9 debug

--- taiga/sketch.js
+++ taiga/sketch.js
@@ -265,7 +265,7 @@ function drawtaiga() {
       let scene4Scroll = scroll4 % (4 * width)
 
       for (let i = 0; i < karinapic.length; i++) {
-        image(karinapic[i], i * width - scroll4, 0);
+        image(karinapic[i], i * width - scene4Scroll, 0);
       }
 
       image(scrollpic, 0, 0)
